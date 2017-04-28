c92e0f4f19b70889bb196c608a7302a5e39c5f5e
LiuinStein PC <liuinstein@outlook.com>
2017-04-29 01:03:19 +0800

    修改Release模式下编译器的优化配置，完善UI

---
+++ DebateTimer/resource.h
@@ -0,0 +1,56 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by DebateTimer.rc
+//
+#define IDD_DEBATETIMER_DIALOG          102
+#define IDR_MAINFRAME                   128
+#define IDD_DIALOG1                     130
+#define IDD_DIALOG2                     132
+#define IDD_DIALOG3                     135
+#define IDR_MENU_SETTING                137
+#define IDC_BUTTON2                     1001
+#define IDC_BTN_RESET_THIS              1002
+#define IDC_BTN_ABOUT                   1004
+#define IDC_LIST1                       1005
+#define IDC_BUTTON1                     1006
+#define IDC_MFCLINK1                    1007
+#define IDC_BTN_SETRULE                 1008
+#define IDC_BTN_RESET_ALL               1009
+#define IDC_BTN_EXIT                    1010
+#define IDC_BTN_LEFT                    1011
+#define IDC_BTN_RIGHT                   1012
+#define IDC_BTN_SHOWLIST                1013
+#define IDC_BTN_SAVE                    1014
+#define IDC_BTN_SAVEAS                  1015
+#define IDC_BTN_IMPORT                  1016
+#define IDC_BTN_SETDEFAULT              1017
+#define IDC_BTN_APPLY                   1018
+#define IDC_BTN_CANCEL                  1019
+#define IDC_EDIT_LIST                   1020
+#define IDC_EDIT_LIST_NUM               1021
+#define IDC_BTN_START                   1022
+#define IDC_BTN_STOP                    1023
+#define IDC_STC_TITLE                   1024
+#define IDC_STC_SHOWTIME                1025
+#define IDC_STC_CHAPTER                 1026
+#define IDC_STC_TIMERNAME               1026
+#define IDC_LIST_SHOWRULELIST           1027
+#define ID_32771                        32771
+#define ID_32772                        32772
+#define ID_32773                        32773
+#define ID_32774                        32774
+#define ID__32775                       32775
+#define ID__32776                       32776
+#define ID__32777                       32777
+#define ID__32778                       32778
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        138
+#define _APS_NEXT_COMMAND_VALUE         32779
+#define _APS_NEXT_CONTROL_VALUE         1028
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
